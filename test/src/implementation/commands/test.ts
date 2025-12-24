d11a29b3b60272de477a1597c2114def6f6673ff
Corno Schraverus <corno@schraverus.com>
2025-12-24 08:43:10 +0100

    ...

--- test/src/implementation/commands/test.ts
+++ test/src/implementation/commands/test.ts
@@ -1,12 +1,12 @@
 import * as _ea from 'exupery-core-alg'
 import * as _easync from 'exupery-core-async'
 
-import * as xxx from "pareto-test/dist/implementation/temp/higher_order_functions/commands/test"
+import { $$ as command_creator } from "pareto-test/dist/implementation/temp/higher_order_functions/commands/test"
 import * as t_generic_testset_to_test_result from "../transformers/generic_testset/test_result"
 
 import * as sh from "pareto-test/dist/implementation/temp/higher_order_functions/generic_testset/testset_shorthands"
 
-export const $$ = xxx.$$(
+export const $$ = command_creator(
     {
         'text to astn': sh.test_group({}),
         'astn to astn': sh.test_group({}),
