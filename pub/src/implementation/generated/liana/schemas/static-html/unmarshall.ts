f2686ee09b0ba7866e86f1a401ae4c72d0e010ab
Corno Schraverus <corno@schraverus.com>
2026-01-26 20:22:57 +0100

    ...

---
+++ pub/src/implementation/generated/liana/schemas/static-html/unmarshall.ts
@@ -0,0 +1,8 @@
+
+import * as _p from "pareto-core/dist/refiner"
+
+import * as t_signatures from "../../../../../interface/generated/liana/schemas/static-html/unmarshall"
+export const Classes: t_signatures.Classes = ($,) => _p.unreachable_code_path()
+export const Phrasing_Content: t_signatures.Phrasing_Content = ($,) => _p.unreachable_code_path()
+export const Flow_Content: t_signatures.Flow_Content = ($,) => _p.unreachable_code_path()
+export const Document: t_signatures.Document = ($,) => _p.unreachable_code_path()
