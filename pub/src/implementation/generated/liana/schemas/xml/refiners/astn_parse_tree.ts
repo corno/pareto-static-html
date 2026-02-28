5533e1ebcc8bead257390e2ceca70ac86ca7e735
Corno Schraverus <corno@schraverus.com>
2026-02-28 18:57:12 +0100

    pdt: ...

--- pub/src/implementation/generated/liana/schemas/xml/refiners/astn_parse_tree.ts
+++ pub/src/implementation/generated/liana/schemas/xml/refiners/astn_parse_tree.ts
@@ -13,7 +13,7 @@ import * as t_out from "../../../../../../interface/generated/liana/schemas/xml/
 
 import * as v_unmarshalled_from_parse_tree from "liana-core/dist/implementation/manual/refiners/unmarshalled/astn_parse_tree"
 
-import * as v_parse_tree_to_location from "astn-core/dist/implementation/manual/transformers/parse_tree/location"
+import * as v_parse_tree_to_location from "astn-core/dist/implementation/manual/transformers/parse_tree/start_token_range"
 
 export const Document: t_signatures.Document = ($, abort) => _p_change_context(
     v_unmarshalled_from_parse_tree.Verbose_Group(
