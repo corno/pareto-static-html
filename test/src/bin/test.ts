c43f81bc0938875e995ad14c8b5d8fd7dd96dc7f
Corno Schraverus <corno@schraverus.com>
2026-04-13 21:06:51 +0200

    pdt: ...

--- test/src/bin/test.ts
+++ test/src/bin/test.ts
@@ -2,7 +2,7 @@
 
 import * as _pn from 'pareto-host-nodejs'
 
-import { $$ as create_test_bin } from "pareto-test/dist/temporary_testset_generator/create_test_bin"
+// import { $$ as create_test_bin } from "pareto-test/dist/temporary_testset_generator/create_test_bin"
 import { $$ as testset } from "../testset"
 
 // _pn.run_main_command(
