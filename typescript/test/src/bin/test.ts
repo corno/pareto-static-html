ab244c367e1570659b725d8b28489a810e17ceb9
Corno Schraverus <corno@schraverus.com>
2026-06-20 10:58:16 +0200

    pdt: ...

--- typescript/test/src/bin/test.ts
+++ typescript/test/src/bin/test.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env -S node --enable-source-maps
 
-import * as ph from 'pareto-host-nodejs'
+import * as p_h from 'pareto-host-nodejs'
 
 // import { $$ as create_test_bin } from "pareto-test/dist/temporary_testset_generator/create_test_bin"
 // import { $$ as testset } from "../testset"
 
-// ph.run_main_command(
+// p_h.run_main_command(
 //     create_test_bin(testset),
 // )
