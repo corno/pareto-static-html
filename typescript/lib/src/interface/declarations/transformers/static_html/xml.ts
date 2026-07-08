2bafcc91868acbf0671ea04b31da0c8f28cb1ff9
Corno Schraverus <corno@schraverus.com>
2026-07-08 21:30:08 +0200

    pdt: ...

--- typescript/lib/src/interface/declarations/transformers/static_html/xml.ts
+++ typescript/lib/src/interface/declarations/transformers/static_html/xml.ts
@@ -4,16 +4,6 @@ import type * as p_ from 'pareto-core/interface/transformer'
 //data types
 import type * as d_in from "../../../generated/liana/schemas/static-html/data.js"
 import type * as d_out from "../../../generated/liana/schemas/xml/data.js"
-import type * as d_text from "pareto-fountain-pen/interface/generated/liana/schemas/text/data"
-
-//dependencies
-import * as t_prose_to_text from "pareto-fountain-pen/implementation/manual/transformers/prose/text"
-
-//shorthands
-import * as sh from "../../../../modules/xml/shorthands/xml/target.js"
-import * as sh_fp from "pareto-fountain-pen/shorthands/prose/deprecated"
-
-
 
 export type Document = p_.Transformer<
     d_in.Document, d_out.Document
