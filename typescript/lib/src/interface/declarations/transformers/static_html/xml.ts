eef641ff3cdfde240deb640c601f7de98dae44c6
Corno Schraverus <corno@schraverus.com>
2026-07-08 14:00:20 +0200

    pdt: ...

--- typescript/lib/src/interface/declarations/transformers/static_html/xml.ts
+++ typescript/lib/src/interface/declarations/transformers/static_html/xml.ts
@@ -15,23 +15,23 @@ import * as sh_fp from "pareto-fountain-pen/shorthands/prose/deprecated"
 
 
 
-    export type Document = p_.Transformer<
-        d_in.Document, d_out.Document
-    >
-    export type Flow_Element = p_.Transformer<
-        d_in.Flow_Element, d_out.Element.content_type.nodes_only.children.L
-    >
-    export type Flow_Content = p_.Transformer<
-        d_in.Flow_Content, d_out.Element.content_type.nodes_only.children
-    >
-    export type Classes = p_.Transformer<
-        d_in.Classes, d_out.Element.attributes
-    >
-    export type Phrasing_Content = p_.Transformer<
-        d_in.Phrasing_Content, d_out.Mixed_Content
-    >
-    export type Phrasing_Element = p_.Transformer<
-        d_in.Phrasing_Element, d_out.Element
-    >
+export type Document = p_.Transformer<
+    d_in.Document, d_out.Document
+>
+export type Flow_Element = p_.Transformer<
+    d_in.Flow_Element, d_out.Element.content_type.nodes_only.children.L
+>
+export type Flow_Content = p_.Transformer<
+    d_in.Flow_Content, d_out.Element.content_type.nodes_only.children
+>
+export type Classes = p_.Transformer<
+    d_in.Classes, d_out.Element.attributes
+>
+export type Phrasing_Content = p_.Transformer<
+    d_in.Phrasing_Content, d_out.Mixed_Content
+>
+export type Phrasing_Element = p_.Transformer<
+    d_in.Phrasing_Element, d_out.Element
+>
 
 
